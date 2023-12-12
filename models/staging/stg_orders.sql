8208312a52a874d2c5a0083f0531518391e8acf3
Shrenik Kole <shrenik.kole@viit.ac.in>
2023-12-12 09:12:59 +0000

    Seeds : added delivery team seed and used it in stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -14,7 +14,9 @@ p.product_id,
 p.product_name,
 p.category,
 p.sub_category,
-{{ markup('order_selling_price', 'order_cost_price') }} as markup
+{{ markup('order_selling_price', 'order_cost_price') }} as markup,
+d.delivery_team
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c on o.customer_id = c.customer_id
 left join {{ ref('raw_product') }} as p on o.product_id = p.product_id
+left join {{ ref('delivery_team') }} as d on o.ship_mode = d.Shipmode
